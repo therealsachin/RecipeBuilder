2bbf7f6a38ac7055742c63052e8a3cb242c8c175
Robin Maben <robinmaben@users.noreply.github.com>
2016-08-19 16:34:17 +0530

    add basic skeleton
    Checked in Django app, models etc.

---
+++ calculator/recipie/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Recipe(models.Model):
+    name = models.TextField(null=False)
+    recipe_text = models.TextField()
+
+
+class Ingredient(models.Model):
+    name = models.TextField(null=False)
+
+
+class NutritionalFactor(models.Model):
+    name = models.TextField(null=False)
+
+
+class IngredientNutritionalValue(models.Model):
+    ingredient = models.ForeignKey(to=Ingredient)
+    nutritional_factor = models.ForeignKey(to=NutritionalFactor)
